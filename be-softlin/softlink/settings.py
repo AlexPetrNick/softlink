a8561cfdd1ddd59b4f88df501f6e8cc73ea0f3e9
Alex Petrov <alexpetrnick@gmail.com>
2021-06-17 19:30:51 +0400

    delete old templates

--- be-softlin/softlink/settings.py
+++ be-softlin/softlink/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'fe-softlink/public/',
+            BASE_DIR / 'templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
