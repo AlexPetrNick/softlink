0aaaae78a81337e498f103fe90a83f19d39c3fd4
Alex Petrov <alexpetrnick@gmail.com>
2021-06-23 08:34:46 +0300

    asfd

---
+++ fe-softlink/src/hok/withCorrectCabinet.js
@@ -0,0 +1,24 @@
+import React from "react"
+import {apiCabinet} from '../apiDAL/DAL'
+
+export const withCorrectCabinet = (Component) => {
+    let CheckId = (props) => {
+        if (props.stateUser.cabinet_id != 0 && props.stateUser.cabinet_id && props.stateCabinet.updateCabinet) {
+            props.updateCabinetAC(false)
+            let temp = fetch("http://127.0.0.1:8000/api/cabinet/" + String(props.stateUser.cabinet_id))
+            temp.then(resp => resp.json())
+            .then(ans => {
+                console.log(ans)
+                props.getStateCabinetAC(ans)
+            })
+            /*apiCabinet.getStateCabinet(props.stateUser.cabinet_id)
+                .then(resp => {
+                    props.getStateCabinetAC(resp)
+                })
+            */
+        }
+            
+        return <Component {...props}/>
+    }
+    return CheckId
+}
