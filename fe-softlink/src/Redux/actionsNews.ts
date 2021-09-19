711beb213f93c791aa1c18114e2508b0aca76149
Alex Petrov <alexpetrnick@gmail.com>
2021-09-19 15:51:07 +0400

    Working typescript

--- fe-softlink/src/Redux/actionsNews.ts
+++ fe-softlink/src/Redux/actionsNews.ts
@@ -1,19 +1,19 @@
 import {apiNews} from '../apiDAL/DAL'
 
-export const SET_STATE_TEXT_TITLE_NEWS = 'SET-STATE-TEXT-TITLE-NEWS'
-export const SET_STATE_TEXT_CONTENT_NEWS = 'SET-STATE-TEXT-CONTENT-NEWS'
-export const ADD_POST = 'ADD-POST'
-export const SET_NEWS_LIST = 'SET-NEWS-LIST'
-export const SET_CATCH = 'SET-CATCH'
-export const SET_COUNT_NEWS = 'SET-COUNT-NEWS'
-export const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
-export const SET_PER_PAGE = 'SET-PER-PAGE'
-export const SET_INIT_COMP_REQUEST = 'SET-INIT-COMP-REQUEST'
-export const TOGGLE_LOAD = 'TOGGLE-LOAD'
-export const SET_COMP_REQUEST_CHANGE_PAGE = 'SET_COMP_REQUEST_CHANGE_PAGE'
+export const SET_STATE_TEXT_TITLE_NEWS:string = 'SET-STATE-TEXT-TITLE-NEWS'
+export const SET_STATE_TEXT_CONTENT_NEWS:string = 'SET-STATE-TEXT-CONTENT-NEWS'
+export const ADD_POST:string = 'ADD-POST'
+export const SET_NEWS_LIST:string = 'SET-NEWS-LIST'
+export const SET_CATCH:string = 'SET-CATCH'
+export const SET_COUNT_NEWS:string = 'SET-COUNT-NEWS'
+export const SET_CURRENT_PAGE:string = 'SET-CURRENT-PAGE'
+export const SET_PER_PAGE:string = 'SET-PER-PAGE'
+export const SET_INIT_COMP_REQUEST:string = 'SET-INIT-COMP-REQUEST'
+export const TOGGLE_LOAD:string = 'TOGGLE-LOAD'
+export const SET_COMP_REQUEST_CHANGE_PAGE:string = 'SET_COMP_REQUEST_CHANGE_PAGE'
 
-export const setStateTextTitleNews = (titleNews) => ({ type: SET_STATE_TEXT_TITLE_NEWS, word: titleNews})
-export const setStateTextContentNews = (contentNews) => ({ type: SET_STATE_TEXT_CONTENT_NEWS, word: contentNews})
+export const setStateTextTitleNews = (titleNews:string):object => ({ type: SET_STATE_TEXT_TITLE_NEWS, word: titleNews})
+export const setStateTextContentNews = (contentNews:string):object => ({ type: SET_STATE_TEXT_CONTENT_NEWS, word: contentNews})
 export const addNews = () => ({ type: ADD_POST })
 export const setNewsListAC = (listNews) => ({ type: SET_NEWS_LIST, listNews })
 export const setCatch = (error) => ({ type: SET_CATCH, error })
