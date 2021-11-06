3353786abb0c52d4bf8cf7a71c9a63b19a54d6fd
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 16:03:57 +0400

    task while 1

--- test.js
+++ test.js
@@ -1,24 +1,12 @@
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+// Setup
+const myArray = [];
 
-function golfScore(par, strokes) {
-    // Only change code below this line
-    let difference = strokes - par
-    console.log(difference)
-    if (strokes === 1) {
-        return names[0]
-    } else if (difference <= -2) {
-        return names[1]
-    } else if (difference === -1) {
-        return names[2]
-    } else if (difference === 0) {
-        return names[3]
-    } else if (difference === 1) {
-        return names[4]
-    } else if (difference === 2) {
-        return names[5]
-    } else if (difference >= 3) {
-        return names[6]
-    }
+// Only change code below this line
+var i = 0
+while (i <= 5) {
+    myArray.push(i)
+    i++
 }
 
-console.log(golfScore(5, 5))
+
+console.log(myArray)
