fcf37ce4e852356228faac6cd058d03845243bb4
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 16:41:57 +0400

    random

--- test.js
+++ test.js
@@ -1,37 +1,8 @@
-// Setup
-const contacts = [
-    {
-        firstName: "Akira",
-        lastName: "Laine",
-        number: "0543236543",
-        likes: ["Pizza", "Coding", "Brownie Points"],
-    },
-    {
-        firstName: "Harry",
-        lastName: "Potter",
-        number: "0994372684",
-        likes: ["Hogwarts", "Magic", "Hagrid"],
-    },
-    {
-        firstName: "Sherlock",
-        lastName: "Holmes",
-        number: "0487345643",
-        likes: ["Intriguing Cases", "Violin"],
-    },
-    {
-        firstName: "Kristian",
-        lastName: "Vos",
-        number: "unknown",
-        likes: ["JavaScript", "Gaming", "Foxes"],
-    },
-];
+function randomFraction() {
 
-function lookUpProfile(name, prop) {
-    let filter = contacts.filter(t => t.firstName === name)
-    if (filter[0][prop]) {
-        return filter[0][prop]
-    }
-    return "No such contact"
+    // Only change code below this line
+
+    return Math.random();
+
+    // Only change code above this line
 }
-
-console.log(lookUpProfile("Akira", "likes"))
