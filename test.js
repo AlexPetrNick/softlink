603d0853cfd84e90ec8af6f936f49e73c5753c1a
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 16:23:54 +0400

    task do while

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-function multiplyAll(arr) {
-    let product = 1;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            product *= arr[i][j]
-        }
-    }
-    return product;
-}
+// Setup
+const myArray = [];
+let i = 10;
 
-console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]))
+// Only change code below this line
+
+do {
+    myArray.push(i)
+    i++;
+} while (i != 11)
+console.log(myArray)
