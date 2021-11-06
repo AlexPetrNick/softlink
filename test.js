54c666ed84516e148537f2351f98028f51056e00
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 16:58:29 +0400

    tern 2

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-function checkEqual(a, b) {
-    return a === b ? "Equal" : "Not Equal"
+function checkSign(num) {
+    return (num > 0) ? "positive" : (num < 0) ? "negative" : "zero"
 }
 
-console.log(checkEqual(1, 2))
+console.log(checkSign(0))
