9e5d7cc50bf283819be7b38f4d923af2a18806ab
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 16:37:55 +0400

    filter

--- test.js
+++ test.js
@@ -1,11 +1,37 @@
-function sum(arr, n) {
-    // Only change code below this line
-    if (n === 0) {
-        return 0
-    } else {
-        return sum(arr, n - 1) + arr[n-1]
+// Setup
+const contacts = [
+    {
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    let filter = contacts.filter(t => t.firstName === name)
+    if (filter[0][prop]) {
+        return filter[0][prop]
     }
-    // Only change code above this line
+    return "No such contact"
 }
 
-console.log(sum([2, 3, 4], 1))
+console.log(lookUpProfile("Akira", "likes"))
