a6a7ce2717ac04d323c3f00d3a348e03909390d4
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 17:32:31 +0400

    codewars 3

--- test.js
+++ test.js
@@ -1,19 +1,12 @@
-function correct(string)
-{
-    let str = ''
-    for (let i of string) {
-        if (i === "5") {
-            str += "S"
-        } else if (i === "1") {
-            str += "I"
-        } else if (i === "0") {
-            str += "O"
-        } else {
-            str += i
-        }
-
+function rentalCarCost(d) {
+    let pay = 40 * d
+    if (d >= 7) {
+        return pay - 50
+    } else if (d >=3) {
+        return pay - 20
+    } else {
+        return pay
     }
-    return str
 }
 
-console.log(correct("51NGAP0RE"))
+console.log(rentalCarCost(3))
