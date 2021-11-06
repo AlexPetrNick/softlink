f5a9bf4f804c2cb673e11a6c3f5bde4256f03a8e
Alex Petrov <alexpetrnick@gmail.com>
2021-11-06 16:48:42 +0400

    random 3

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-function randomWholeNum() {
-
+function randomRange(myMin, myMax) {
     // Only change code below this line
-
-    return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
+    // Only change code above this line
 }
